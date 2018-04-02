a8f5073315fd3788dd309ec74a3546168a62161c
wangyu21 <wangyu21@jd.com>
2018-04-02 17:45:40 +0800

    test

--- src/main/java/com/wangyu/spring/utils/SpringInterfaceTest.java
+++ src/main/java/com/wangyu/spring/utils/SpringInterfaceTest.java
@@ -1,10 +1,13 @@
 package com.wangyu.spring.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Service;
 
+import java.lang.annotation.Annotation;
 import java.util.Iterator;
 
 /**
@@ -13,11 +16,23 @@ import java.util.Iterator;
 @Service
 public class SpringInterfaceTest implements BeanFactoryPostProcessor {
 
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        Iterator<String> clazzNameIterator = configurableListableBeanFactory .getBeanNamesIterator();
-        while (clazzNameIterator.hasNext()){
-            System.out.println(clazzNameIterator.next());
 
+    private static Logger log = LoggerFactory.getLogger(SpringInterfaceTest.class);
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        try{
+            Iterator<String> clazzNameIterator = configurableListableBeanFactory .getBeanNamesIterator();
+            while (clazzNameIterator.hasNext()){
+                String clazzName = clazzNameIterator.next();
+                System.out.println(clazzName);
+                Class c = Class.forName(clazzName);
+                Annotation[] annotations = c.getAnnotations();
+                for(Annotation annotation : annotations){
+                    System.out.println(annotation + "----");
+                }
+            }
+        }catch (Exception e){
+            log.error("--", e);
         }
+
     }
 }
